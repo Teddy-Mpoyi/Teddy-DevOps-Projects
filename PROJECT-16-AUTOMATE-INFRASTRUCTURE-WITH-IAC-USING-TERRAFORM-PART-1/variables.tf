5bcbf2b32d33d587c8822083257602dc7bda7284
Teddy T Mpoyi <103863428+Teddy-Mpoyi@users.noreply.github.com>
2024-03-01 11:14:56 -0500

    Update variables.tf

--- PROJECT-16-AUTOMATE-INFRASTRUCTURE-WITH-IAC-USING-TERRAFORM-PART-1/variables.tf
+++ PROJECT-16-AUTOMATE-INFRASTRUCTURE-WITH-IAC-USING-TERRAFORM-PART-1/variables.tf
@@ -1,1 +1,28 @@
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+    default = null
+}
 
