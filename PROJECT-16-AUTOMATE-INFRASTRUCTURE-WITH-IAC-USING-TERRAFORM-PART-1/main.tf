d7598716b93c4d3023a358bdf102c49f210043aa
Teddy T Mpoyi <103863428+Teddy-Mpoyi@users.noreply.github.com>
2024-03-01 11:13:06 -0500

    Update main.tf

--- PROJECT-16-AUTOMATE-INFRASTRUCTURE-WITH-IAC-USING-TERRAFORM-PART-1/main.tf
+++ PROJECT-16-AUTOMATE-INFRASTRUCTURE-WITH-IAC-USING-TERRAFORM-PART-1/main.tf
@@ -1,1 +1,42 @@
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "additional_tags" {
+  default     = {}
+  description = "Additional resource tags"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support 
+  enable_dns_hostnames           = var.enable_dns_support
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink
+tags = {
+  Name = "Main VPC"
+}
+}
+
+# Create public subnets
+resource "aws_subnet" "public" {
+  count  = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets   
+  vpc_id = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4 , count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+tags = merge(
+    var.additional_tags,
+    {
+      Name = format("PrivateSubnet-%s", count.index)
+    } 
+  )
+}
 
